8a7cc23d17735d52580bc60c86bfcf20309ec47e
wingsmm <wingsmm@me.com>
2015-04-05 21:38:27 +0800

    no message

---
+++ GuessFinger/GuessFinger WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  GuessFinger WatchKit Extension
+//
+//  Created by ZhangBo on 15/4/5.
+//  Copyright (c) 2015年 wingsmm. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ GuessFinger/GuessFinger/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GuessFinger
+//
+//  Created by ZhangBo on 15/4/5.
+//  Copyright (c) 2015年 wingsmm. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
